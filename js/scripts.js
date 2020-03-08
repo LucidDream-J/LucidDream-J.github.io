d2b3ffb63755a1c7c847de6c39ad6877c8494752
LucidDream-J <48848408+LucidDream-J@users.noreply.github.com>
2020-03-08 10:23:17 +0800

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+
+
+const floatingBtn = document.querySelector('.footerBtn');
+
+
+floatingBtn.addEventListener('click', (e) => {
+
+    const footer = document.querySelector('.footer');
+
+    if (footer.classList.contains('active')) {
+
+
+        
+        footer.classList.remove('active');
+        e.target.innerText = "Terms, Privacy, Currency & more";
+
+    } else {
+
+        footer.classList.add('active');
+        e.target.innerText = "Close  X";
+    }
+
+})
